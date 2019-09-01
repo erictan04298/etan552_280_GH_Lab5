3f80a517a92ef004e313f4071c404da6d22d172f
User <etan552@aucklanduni.ac.nz>
2019-09-01 15:41:38 +1200

    push lab2 file to remote repo

---
+++ test_maths.py
@@ -0,0 +1,25 @@
+import unittest     # Import the Python unit testing framework
+import maths        # Our code to test
+
+
+class MathsTest(unittest.TestCase):
+    ''' Unit tests for our maths functions. '''
+
+    def test_add(self):
+        ''' Tests the add function. '''
+        result = maths.add(2, 6, 2)
+        self.assertEqual(result, "1000", "")
+
+    def test_fibonacci(self):
+        ''' Tests the fibonacci function. '''
+        result =  maths.fibonacci(5)
+        self.assertEqual(result, 8, "")
+    
+    def test_convert_base(self):
+        result = maths.convert_base(8, 2)
+        self.assertEqual(result,"1000", "")
+
+
+# This allows running the unit tests from the command line (python test_maths.py)
+if __name__ == '__main__':
+    unittest.main()
